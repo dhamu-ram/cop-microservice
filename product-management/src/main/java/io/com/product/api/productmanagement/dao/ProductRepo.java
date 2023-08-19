b65bc9d0bc01f567d1f56b69daab5adc1d8ba605
Dhamodharan Ramalingam <dhamu.sciera@gmail.com>
2023-08-19 08:58:09 -0700

    product code fix

--- product-management/src/main/java/io/com/product/api/productmanagement/dao/ProductRepo.java
+++ product-management/src/main/java/io/com/product/api/productmanagement/dao/ProductRepo.java
@@ -3,7 +3,9 @@ package io.com.product.api.productmanagement.dao;
 import io.com.product.api.productmanagement.model.Product;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-
+/**
+ * ProductRepo
+ */
 public interface ProductRepo extends MongoRepository<Product, Integer> {
 
 }
